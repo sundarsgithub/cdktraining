dc278f8f9b1a34004bc891749f9a05d7619762e6
unknown <sundar>
2025-01-08 03:52:25 +0530

    new updates

--- my-ec2/bin/my-ec2.ts
+++ my-ec2/bin/my-ec2.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MyEc2Stack } from '../lib/my-ec2-stack';
+import { sundarEc2Stack } from '../lib/my-ec2-stack';
 
 const app = new cdk.App();
-new MyEc2Stack(app, 'MyEc2Stack', {
+new sundarEc2Stack(app, 'sundarEc2Stack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -16,6 +16,8 @@ new MyEc2Stack(app, 'MyEc2Stack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
+
+  env: { account:'992382386705', region: 'us-east-1'}
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- my-ec2/lib/my-ec2-stack.ts
+++ my-ec2/lib/my-ec2-stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MyEc2Stack extends cdk.Stack {
+export class sundarEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -12,5 +13,22 @@ export class MyEc2Stack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MyEc2Queue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // define default vpc to use from existing aws region 
+    const vpc = ec2.Vpc.fromLookup(this,'ExistingVPC' ,{
+      isDefault: true
+  });
+  // creating ec2 instace 
+  const sundarinstance = new ec2.Instance(this,'sundarmachine',{
+
+      vpc,  
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyName: 'splunk-key',
+  
+  });
+
+ 
+
   }
 }
