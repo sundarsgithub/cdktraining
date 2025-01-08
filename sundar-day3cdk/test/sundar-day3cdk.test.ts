389e27351c3c7753bce94ec4c7dd589fd5c27e8d
unknown <sundar>
2025-01-08 23:48:53 +0530

    updates

---
+++ sundar-day3cdk/test/sundar-day3cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SundarDay3Cdk from '../lib/sundar-day3cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sundar-day3cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SundarDay3Cdk.SundarDay3CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
